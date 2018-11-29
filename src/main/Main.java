4ee12ca057619c67417f3675c7ba7a63ae7989c4
Usuario <Usuario@10.22.148.93>
2018-11-29 14:44:57 +0000

    Kata5 Parte 2: Creacion de una tabla EMAIL en una bd con una serie de campos.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,5 @@
 package main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.*;
 public class Main {
     
